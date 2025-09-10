ac4256e35464fe20e2b0c8087a1ef4e4192de244
Vedant <vedant.nipane@students.iiit.ac.in>
2025-09-10 23:20:18 +0530

    project structure ready env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vedant Nipane",
+    author_email="vedantliladharnipane012@gmail.com",
+    packages=find_packages()
+)
